62ea1a32f253f717925e7207a4e9998415f49758
KoharuL <142435719+KoharuL@users.noreply.github.com>
2024-01-28 01:42:25 +0700

    update template

---
+++ Plugins/Angel.plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @name Angel
+ * @author KoharuL
+ */
+
+var root = document.querySelector(':root') ;
+
+function setOverlay() {
+    root.style.setProperty('--transparent', 'rgba(180, 0, 0, 0.6)') ;
+}
